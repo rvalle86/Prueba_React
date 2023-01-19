0c64c5c67f52058348896b0a4961eda47de697fa
Robert Magdiel Valle Alvarado <rvalle@bancatlan.hn>
2023-01-19 17:09:13 -0600

    prueba

--- src/App.js
+++ src/App.js
@@ -5,18 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <form>
+          <label>Pantalla Inicial</label><br></br>
+            <input type="text" name="ciudad" placeholder='Ingrese Ciudad' />
+          <input type="submit" name='ubi' value="Usar mi ubicación" />
+          <input type="submit" name='envi' value="Buscar" />
+          
+        </form>
       </header>
     </div>
   );
